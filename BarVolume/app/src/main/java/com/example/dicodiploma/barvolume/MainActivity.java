c5a1dce5393fd92ce8ff062625ce813b6043f499
dianynf <dianbugas@gmail.com>
2019-11-13 11:02:35 +0700

    barVolum sukses

--- BarVolume/app/src/main/java/com/example/dicodiploma/barvolume/MainActivity.java
+++ BarVolume/app/src/main/java/com/example/dicodiploma/barvolume/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnCalculate;
     TextView tvResult;
 
+    private static final String STATE_RESULT = "state_result";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnCalculate = findViewById(R.id.btn_calculate);
         tvResult = findViewById(R.id.tv_result);
 
+        btnCalculate.setOnClickListener(this);
+
         if (savedInstanceState != null) {
             String result = savedInstanceState.getString(STATE_RESULT);
             tvResult.setText(result);
@@ -32,70 +36,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_RESULT, tvResult.getText().toString());
+    }
+
+    @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_calculate) {
-            //trim() untuk menghiraukan spasi
-            //.toString()untuk mengambil isi dari sebuah EditText
             String inputLength = edtLength.getText().toString().trim();
             String inputWidth = edtWidth.getText().toString().trim();
             String inputHeight = edtHeight.getText().toString().trim();
 
             boolean isEmptyFields = false;
-            boolean isInvalidDouble = false;
 
-            //isEmpty untuk cek apakah inputan kosong
+            /*
+            Validasi apakah inputan masih ada yang kosong
+             */
             if (TextUtils.isEmpty(inputLength)) {
                 isEmptyFields = true;
                 edtLength.setError("Field ini tidak boleh kosong");
             }
+
             if (TextUtils.isEmpty(inputWidth)) {
                 isEmptyFields = true;
                 edtWidth.setError("Field ini tidak boleh kosong");
             }
+
             if (TextUtils.isEmpty(inputHeight)) {
                 isEmptyFields = true;
                 edtHeight.setError("Field ini tidak boleh kosong");
             }
 
-            //vaiabel input yg bukan angka
-            Double length = toDouble(inputLength);
-            Double width = toDouble(inputWidth);
-            Double height = toDouble(inputHeight);
-            if (length == null) {
-                isInvalidDouble = true;
-                edtLength.setError("Field ini harus berupa nomer yang valid");
-            }
-            if (width == null) {
-                isInvalidDouble = true;
-                edtWidth.setError("Field ini harus berupa nomer yang valid");
-            }
-            if (height == null) {
-                isInvalidDouble = true;
-                edtHeight.setError("Field ini harus berupa nomer yang valid");
-            }
-            //untuk menampilkan data ke edit
-            if (!isEmptyFields && !isInvalidDouble) {
-                double volume = length * width * height;
+            /*
+            Jika semua inputan valid maka tampilkan hasilnya
+             */
+            if (!isEmptyFields) {
+                double volume = Double.valueOf(inputLength) * Double.valueOf(inputWidth) * Double.valueOf(inputHeight);
                 tvResult.setText(String.valueOf(volume));
             }
-
         }
-    }
-
-    private Double toDouble(String str) {
-        try {
-            return Double.valueOf(str);
-        } catch (NumberFormatException e) {
-            return null;
-        }
-    }
-
-    //code untuk perintah lanscape
-    private static final String STATE_RESULT = "state_result";
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(STATE_RESULT, tvResult.getText().toString());
     }
 }
